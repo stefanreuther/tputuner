3c000c51cd06414e14433ac7b250f87095d3499a
Stefan Reuther <streu@gmx.de>
1999-03-17 21:30:32 +0000

    register allocation xchg+mov peephole mov+op+mov => op peephole push+mov peephole shl+shl => shl peephole

--- dfa.h
+++ dfa.h
@@ -10,6 +10,7 @@
 
 void data_flow_analysis(CInstruction* insn);
 bool flag_check(CInstruction* insn);
+bool can_modify_reg(CInstruction* p, TRegister r);
 void make_backlinks(CInstruction* insn);
 
 #endif
