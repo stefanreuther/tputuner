2589a03dd042ce7d517bc7fa6958dc5b05517dde
Stefan Reuther <streu@gmx.de>
2000-06-18 10:07:14 +0000

    bubble up moves to make them unnecessary (undo register dancing) small fixes to CSE

--- cse.h
+++ cse.h
@@ -1,8 +1,23 @@
 #ifndef CSE_H
 #define CSE_H
 
+#include <vector>
 #include "insn.h"
 
+struct OperandSet {
+    int regs;
+    vector<CArgument*> mem;
+    bool flags, stack;
+    
+    OperandSet() : regs(0), flags(false), stack(false) { }
+    void fix_regs();
+    void fix_regs_conservative();
+    void add_op(CArgument* a);
+    bool contains_arg(CArgument* a);
+};
+
 void do_cse(CInstruction* insn);
+void compute_insn_dep(OperandSet& in, OperandSet& out, CInstruction* p);
+bool is_break(CInstruction* p);
 
 #endif
